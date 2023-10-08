89c7c182a0c472e5e1ecca944ef4edd4eaaf8142
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-07 22:15:30 -0400

    create and rendering svelte component to greeting

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ''
+
+  if (currentHour < 12) {
+    greeting = 'Good Morning'
+  } else if (currentHour < 18) {
+    greeting = 'Good Afternoon'
+  } else {
+    greeting = 'Good Evening'
+  }
+</script>
+
+<h1 class="text-4xl font-bold select-none">{greeting}</h1>
