4c799c16652a3905150c80547db0282a2e554e5f
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-10 22:36:40 -0400

    component of play button of the cards musics

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,36 @@
+import { Pause, Play } from './Player'
+import { usePlayerStore } from '@/store/playerStore'
+
+export function CardPlayButton({ id }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
+    usePlayerStore(state => state)
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+
+        setIsPlaying(true)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+
+        console.log({ songs, playlist })
+      })
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'
+    >
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  )
+}
