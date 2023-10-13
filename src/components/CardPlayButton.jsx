85350f71c9e720be7e5fd8e5cf124beac5f97487
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-13 10:35:27 -0400

    change the colors of the buttons and sliders

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -26,7 +26,7 @@ export function CardPlayButton({ id }) {
   return (
     <button
       onClick={handleClick}
-      className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'
+      className='card-play-button rounded-full bg-[#1ed760] hover:bg-[#1fdf64] p-4 hover:scale-105 transition shadow-md'
     >
       {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
