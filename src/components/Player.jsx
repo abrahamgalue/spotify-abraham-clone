9ab3ce24fdef064b0373075c7c160bb8b6c4f499
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-13 10:34:01 -0400

    add song control

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from '@/store/playerStore'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Slider } from './Slider'
 
 export const Pause = () => (
@@ -48,7 +48,7 @@ export const Volume = () => (
 const CurrentSong = ({ image, title, artists }) => {
   return (
     <div className='flex items-center gap-5 relative overflow-hidden'>
-      <picture className='w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden'>
+      <picture className='w-16 h-16 bg-zinc-800 rounded shadow-lg overflow-hidden'>
         <img src={image} alt={title} />
       </picture>
 
@@ -56,6 +56,56 @@ const CurrentSong = ({ image, title, artists }) => {
         <h3 className='font-semibold text-sm block'>{title}</h3>
         <span className='text-xs opacity-80'>{artists?.join(', ')}</span>
       </div>
+    </div>
+  )
+}
+
+const SongControl = ({ audio }) => {
+  const [currentTime, setCurrentTime] = useState(0)
+
+  useEffect(() => {
+    audio.current.addEventListener('timeupdate', handleTimeUpdate)
+
+    return () => {
+      audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+    }
+  }, [])
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audio.current.currentTime)
+  }
+
+  const formatTime = time => {
+    if (time == null) return '0:00'
+
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time / 60)
+
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  const duration = audio?.current?.duration ?? 0
+
+  return (
+    <div className='flex gap-x-2 text-xs pt-2'>
+      <span className='opacity-50 w-12 text-right'>
+        {formatTime(currentTime)}
+      </span>
+
+      <Slider
+        max={audio?.current?.duration ?? 0}
+        min={0}
+        value={[currentTime]}
+        className='w-[400px]'
+        onValueChange={value => {
+          const [newCurrentTime] = value
+          audio.current.currentTime = newCurrentTime
+        }}
+      />
+
+      <span className='opacity-50 w-12'>
+        {duration ? formatTime(duration) : '0:00'}
+      </span>
     </div>
   )
 }
@@ -130,16 +180,18 @@ export function Player() {
   }
 
   return (
-    <div className='flex flex-row justify-between w-full px-4 py-3 z-50'>
+    <div className='flex flex-row justify-between w-full px-1 py-3 z-50'>
       <div className='w-[200px]'>
         <CurrentSong {...currentMusic.song} />
       </div>
 
-      <div className='grid place-content-center gap-4 flex-1 pt-2'>
-        <div className='flex justify-center'>
+      <div className='grid place-content-center gap-4 flex-1'>
+        <div className='flex justify-center flex-col items-center'>
           <button className='bg-white rounded-full p-2' onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
+          <SongControl audio={audioRef} />
+          <audio ref={audioRef} />
         </div>
       </div>
 
